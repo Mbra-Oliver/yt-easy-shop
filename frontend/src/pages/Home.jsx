fb5d41a8ec36e8140b7a62031c309108904b3067
Mbra Oliver <mbraoliver28@gmail.com>
2024-02-07 12:11:47 +0000

    Page d'accueil

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import HeroSection from "../components/Hero";
+
+import ProductsList from "../components/Products/ProductsList";
+import CategoriesList from "../components/Categories/CategoriesList";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <HeroSection />
+
+      {/* Les catégories */}
+
+      <CategoriesList />
+
+      {/* Lister les articles */}
+
+      <ProductsList />
+    </>
+  );
 }
